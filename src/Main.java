34464b295f313bb641549f60aae83acc84992f05
Лидия Дворкина <lideya832@mail.ru>
2022-10-18 20:27:26 +0300

    Прошу объяснить мне как работает StringBulder, я не понимаю и соответсвенного не могу решить задачу.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+        String[] number = {"один", "два", "три", "один", "два"};
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < number.length; i++) {
+            if (!builder.contains(number[j]))
+            {builder.append(number[j]).append(" ")}
+            String another = number [i];
+            for (int j = i + 1; j< number.length; j++){
+                if (another.equals(number[j])){
+                    System.out.print(number [i] + " ");}
+
+
+
+
+
+
 }
+
+                    }
+                }
+        }
+
+
+
+
+
+
+
+
