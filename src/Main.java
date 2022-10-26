ab285ac02c03e2780811c5b163cf6e358faec309
Лидия Дворкина <lideya832@mail.ru>
2022-10-26 18:21:49 +0300

    Задача из билета № 4

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,27 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
         String[] number = {"один", "два", "три", "один", "два"};
         StringBuilder builder = new StringBuilder();
+              for (int i = 0; i < number.length; i++) {
+               String another = number [i];
+               boolean dublicateFound = false;
+               for (int j = i + 1; j < number.length; j++) {
+               if (number[j].equals(number[i])) {
+                   dublicateFound = true;
+                   break;}
+               }
+               if (builder.length()>0) {
+                   builder.append(' ');
+               }
+               if (!dublicateFound) {
+                   builder.append(number[i]);
+               }
+              }
+        System.out.println(builder);
+    }}
 
-        for (int i = 0; i < number.length; i++) {
-            if (!builder.contains(number[j]))
-            {builder.append(number[j]).append(" ")}
-            String another = number [i];
-            for (int j = i + 1; j< number.length; j++){
-                if (another.equals(number[j])){
-                    System.out.print(number [i] + " ");}
 
-
-
-
-
-
-}
-
-                    }
-                }
-        }
 
 
 
